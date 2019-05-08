83ee9f5954f97134729e841ece1298e2628345e6
ranode <botnex@naver.com>
2019-05-08 16:30:29 +0900

    Add Code

--- Calculator.java
+++ Calculator.java
@@ -4,6 +4,14 @@ public class Calculator {
 
     public static void main (String[] args){
 
+      int a = 4;
+      int b = 2;
+
+      System.out.println( sum(a,b) );
+
     }
 
+    private static int sum (int a, int b) {
+      return a + b;
+    }
 }
